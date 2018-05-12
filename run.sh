e8a076c9f72024dd7f3887eaab1dc21583d1c0f1
churchmf <churchmf@gmail.com>
2018-05-12 13:13:13 -0600

    Restart php5 fpm and run pikrellcam

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "restarting php5-fpm..."
+sudo service php5-fpm restart
 echo "restarting nginx..."
 sudo service nginx restart
+echo "starting pikrellcam"
+sudo /home/pi/pikrellcam/pikrellcam &
